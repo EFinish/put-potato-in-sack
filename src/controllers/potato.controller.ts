c985c3682fe4e95cec9f56944d11f3092a44647a
EFinish <EFinish@protonmail.com>
2023-01-27 11:22:25 +0100

    basic controller setup

--- src/controllers/potato.controller.ts
+++ src/controllers/potato.controller.ts
@@ -1,5 +1,7 @@
 
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { CreatePotatoDto } from 'src/dtos/create-potato.dto';
 import { PotatoService } from '../services/potato.service';
 
 @Controller('potato')
@@ -7,17 +9,23 @@ export class PotatoController {
 constructor(private readonly potatoService: PotatoService) {}
 
   @Get()
-  findAll(): string {
-    return this.potatoService.getPotatos()
+  findAll(@Res() res: Response){
+    const result = this.potatoService.getPotatos()
+
+    res.status(HttpStatus.OK).json(result);
   }
 
   @Post()
-  create(): string {
-    return this.potatoService.createPotato()
+  create(@Body() createCatDto: CreatePotatoDto, @Res() res: Response) {
+    const result = this.potatoService.createPotato();
+
+    res.status(HttpStatus.CREATED).send();
   }
 
   @Post(':sack-id')
-  putPotatoInSack(): string {
-    return this.potatoService.putPotatoInSack()
+  putPotatoInSack(@Param('sack-id') id: string, @Res() res: Response) {
+    const result = this.potatoService.putPotatoInSack()
+
+    res.status(HttpStatus.OK).send();
   }
 }
---
+++ src/dtos/create-potato.dto.ts
@@ -0,0 +1,7 @@
+
+export class CreatePotatoDto {
+    name: string;
+    color: string;
+    weight: number;
+  }
+  
