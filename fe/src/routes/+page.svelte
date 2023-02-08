63eedd1c5de421183312328a0ba0471caa68943a
EFinish <EFinish@protonmail.com>
2023-02-08 16:05:44 +0100

    fe skeleton using svelte, reconfig docker setup

---
+++ fe/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Sacks from '../lib/components/Sacks.svelte';
+    import VegetablePile from '../lib/components/VegetablePile.svelte';
+</script>
+
+<VegetablePile />
+<Sacks />
+
+<div class="px-2 sm:px-4 py-2.5 w-full">
+    <div class="mx-auto container">
+        <slot />
+    </div>
+</div>
