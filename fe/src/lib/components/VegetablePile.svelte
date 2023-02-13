dcab48067a2ac6123affcc725827e83b97a49f4f
EFinish <EFinish@protonmail.com>
2023-02-13 12:31:09 +0100

    finally got the beautiful potato visible

--- fe/src/lib/components/VegetablePile.svelte
+++ fe/src/lib/components/VegetablePile.svelte
@@ -2,14 +2,14 @@
 	import type { Potato } from '$lib/models/potato';
 	export let potatoes: Potato[] = [];
 
-    let src = `${process.env.PUBLIC_URL}/potato.png`
+	import potatoImage from "$lib/assets/images/potato.png"
 </script>
 
 <h1>Vegetable Pile!</h1>
 
 {#each potatoes as potato (potato.id)}
 	<div>
-		<img alt="a potato" {src} >
+		<img alt="a potato" src={potatoImage} >
 		<div>{potato.name}</div>
 		<div>{potato.weight}</div>
 	</div>
